aac57844a10903f6d7eaf47bfbe9d632cdd85127
Arne <mail@arnefeil.de>
2017-05-11 10:00:07 +0200

    restore url.js

--- app/api/url.js
+++ app/api/url.js
@@ -1,7 +1,6 @@
 // @flow
-const url: string = __DEV__ //|| true
-  ? 'http://192.168.0.164/liga-tool'
-  // ? 'https://dev.kicker-hh.de/de/competitions'
+const url: string = __DEV__
+  ? 'https://dev.kicker-hh.de/de/competitions'
   : 'https://kickern-hamburg.de/de/competitions';
 
 export default url;
