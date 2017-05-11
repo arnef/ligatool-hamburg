d49eba6fe06354bbb0e86ea7c46e6037204f09b5
Arne <mail@arnefeil.de>
2017-05-11 14:54:26 +0200

    update api url

--- app/api/api.js
+++ app/api/api.js
@@ -2,13 +2,6 @@
 import { create } from 'apisauce';
 import baseUrl from './url';
 
-// export const baseUrl: string = __DEV__
-//   ? 'http://192.168.0.164/liga-tool'
-//   // ? 'http://192.168.1.2/liga-tool'
-//   // ? 'http://localhost/liga-tool'
-//   // ? 'https://dev.kicker-hh.de/de/competitions'
-//   : 'https://kickern-hamburg.de/de/competitions';
-
 const api: API = create({
   baseURL: `${baseUrl}/index.php?option=com_sportsmanagerapi&q=`
 });
