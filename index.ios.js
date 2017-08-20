4d26f2868e0bd3468e9d2923df8e06271e8347f9
Arne <mail@arnefeil.de>
2017-08-20 09:56:09 +0200

    chore: upgrade rn and dependencies
    app now runs with rn0.47. you have rebuild the project and maybe run
    watchmen watch-del-all
    rm -rf node_modules/
    rm -rf $TMPDIR/react-*
    yarn cache clean
    yarn install
    yarn start -- --reset-cache
    I had to downgrade babel-reset-react-native@2.0.0 because there is an issue with version 3.0.0
    https://github.com/facebook/react-native/issues/14838
    
    closes #41

--- index.ios.js
+++ index.ios.js
@@ -4,11 +4,10 @@ import { AppRegistry, StatusBar } from 'react-native';
 import App from './app/App';
 
 import { Sentry } from 'react-native-sentry';
-if (!__DEV__) {
-  Sentry.config(
-    'https://9a666a3137a240fb8b41520b97d47d29:bf46d5a18e794b95abe9b576f4d44096@sentry.io/196572',
-  ).install();
-}
+
+Sentry.config(
+  'https://9a666a3137a240fb8b41520b97d47d29:bf46d5a18e794b95abe9b576f4d44096@sentry.io/196572',
+).install();
 
 class androidapp extends Component {
   componentWillMount() {
